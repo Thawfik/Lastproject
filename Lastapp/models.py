abb19f6be8d2e256ea23bd6c0775aac145b1a037
Thawfik <ayanathawfik7@gmail.com>
2025-02-04 23:02:15 +0000

    new file:   Lastapp/__pycache__/__init__.cpython-312.pyc 	new file:   Lastapp/__pycache__/admin.cpython-312.pyc 	new file:   Lastapp/__pycache__/apps.cpython-312.pyc 	new file:   Lastapp/__pycache__/models.cpython-312.pyc 	new file:   Lastapp/migrations/0001_initial.py 	new file:   Lastapp/migrations/__pycache__/0001_initial.cpython-312.pyc 	new file:   Lastapp/migrations/__pycache__/__init__.cpython-312.pyc 	modified:   Lastapp/models.py 	modified:   Lastproject/__pycache__/settings.cpython-312.pyc 	new file:   Lastproject/__pycache__/urls.cpython-312.pyc

--- Lastapp/models.py
+++ Lastapp/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Categorie(models.Model):
+    titre = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+class Produit(models.Model):
+    nom = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    prix_unitaire = models.FloatField(null=True)
+    quantité_stock = models.IntegerField(null=True)
+    Statut = models.CharField(max_length=100)
+    Categorie = models.ForeignKey(Categorie , on_delete=models.CASCADE)
+
+class Client(models.Model):
+    nom = models.CharField(max_length=255)
+    prenom = models.CharField(max_length=255)
+    contact = models.CharField(max_length=255)
+    residence = models.CharField(max_length=255)
+
+class Panier(models.Model):
+    date_creation = models.DateField()
+    etat = models.CharField(max_length=100)
+
+class Achat(models.Model):
+    quantité_total = models.IntegerField(null=False)
+    prix_total = models.FloatField(null=False)
+    Statut = models.CharField(max_length=255)
+    Client = models.ForeignKey(Client , on_delete=models.CASCADE)
+    Panier = models .ForeignKey(Panier , on_delete=models.CASCADE)
+
+class Transaction(models.Model):
+    operation = models.CharField(max_length=100)
+    montant = models.FloatField(null=False)
+    operateur = models.CharField(max_length=255)
+    dateoperation = models.DateField()
+    Client = models.ForeignKey(Client , on_delete=models.CASCADE)
+
+class Facture(models.Model):
+    date = models.DateField()
+    mode_paiement = models.CharField(max_length=100)
+    etat = models.CharField(max_length=255 )
+    notes = models.CharField(max_length=255)
+    Panier = models.ForeignKey(Panier , on_delete=models.CASCADE)
+    
